50ab0ac594509f834b44f7b49c477cfab0d95e6b
k2345rftf <36264105+k2345rftf@users.noreply.github.com>
2018-05-06 10:55:40 +0800

    Add files via upload

--- database.py
+++ database.py
@@ -12,6 +12,16 @@ BASE = Base
 
 
 
+
+
+
+# 0 - gardener
+# 1 - casher
+# 2 - accountent
+# 3 - director
+# 4 - hired worker
+
+
 class User(Base):
     """table of authorization"""
     __tablename__ = 'user'
@@ -56,17 +66,19 @@ class CounterUnit(Base):
     classAccur = Column(Float)
 
 
-    def __init(self):
+    def __init(self, number, user_id, dateUnstCount, typeCounter, classAccur):
+        self.number = number
         self.user_id = user_id
         self.dateUnstCount = dateUnstCount
         self.typeCounter = typeCounter
         self.classAccur = classAccur
 
     def __repr__(self):
-        return "<{}(user_id = {}, dateUnstCount = {}, typeCounter = {}, classAccur = {}". format(self.user_id,
-                                                                                                self.dateUnstCount,
-                                                                                                self.typeCounter,
-                                                                                                self.classAccur)
+        return "<{}(number = {}, user_id = {}, dateUnstCount = {}, typeCounter = {}, classAccur = {}". format(  self.number,
+                                                                                                                self.user_id,
+                                                                                                                self.dateUnstCount,
+                                                                                                                self.typeCounter,
+                                                                                                                self.classAccur)
 
 
 
@@ -167,7 +179,7 @@ class Service(Base):
         self.peny = peny
         self.unit = unit
     def __repr__(self):
-        return "<date={}, name_serv={}, doc_serv={}, period ={}, cost_unit={}, unit={}, peny_data={}, peny={}, unit = {}".format(self.__class__, self.date,
+        return "<{}(date={}, name_serv={}, doc_serv={}, period ={}, cost_unit={}, unit={}, peny_data={}, peny={}, unit = {}".format(self.__class__, self.date,
                                                                                                                                                 self.name_serv,
                                                                                                                                                 self.doc_serv,
                                                                                                                                                 self.period,
@@ -189,7 +201,7 @@ class Counter(Base):
 
     service = relationship("Service", backref="name_serv")
 
-    def __init__(self, date, time, id_counter, user_id, name_counter, value):
+    def __init__(self, date, id_counter, user_id, name_counter, value):
         self.date = date
         self.id_counter = id_counter
         self.user_id = user_id
@@ -197,11 +209,65 @@ class Counter(Base):
         self.value = value
 
     def __repr__(self):
-        return "<date={}, id_counter={}, user_id={}, name_counter={}, value={}".format(self.__class__, self.date,
+        return "<{}(date={}, id_counter={}, user_id={}, name_counter={}, value={}".format(self.__class__, self.date,
                                                                                                         self.id_counter,
                                                                                                         self.user_id,
                                                                                                         self.name_counter,
                                                                                                         self.value)
+
+
+
+class Inventory(Base):
+    __tablename__ = "Inventory"
+    date = Column(Date)
+    user_id = Column(Integer)
+    number_unit = Column(Integer, primary_key = True)
+    name_unit = Column(String)
+    available = Column(Integer)
+
+    def __init__(self, date, user_id, number_unit, name_unit, available):
+        self.date = date
+        self.user_id = 
+        self.number_unit = number_unit
+        self.name_unit = name_unit
+        self.available = available
+
+    def __repr__(self):
+        return "<{}(date = {}, user_id = {}, number_unit = {}, name_unit = {}, available = {}".format(self.__class__,
+                                                                                                        self.date,
+                                                                                                        self.user_id,
+                                                                                                        self.number_unit,
+                                                                                                        self.name_unit,
+                                                                                                        self.available)
+
+
+
+class Company(Base):
+    __tablename__ = "trans_copm"
+    id_transaction = Column(Integer, primary_key = True)
+    date = Column(Date)
+    id_creditor = Column(Integer)
+    name_service = Column(String)
+    cost = Column(Float)
+    id_deb = Column(Integer)
+
+    def __init__(self, id_transaction, date, name_creditor, name_service, cost, name_deb):
+        self.id_transaction = id_transaction
+        self.date = date
+        self.id_creditor = id_creditor
+        self.name_service = name_service
+        self.cost = cost
+        self.id_deb = id_deb
+
+    def __repr__(self):
+        return "<{}(id_transaction={}, date={}, id_creditor={}, name_service={}, cost={}, id_deb = {}".format(self.__class__, 
+                                                                                                            self.id_transaction,
+                                                                                                            self.date,
+                                                                                                            self.id_creditor,
+                                                                                                            self.name_service,
+                                                                                                            self.cost,
+                                                                                                            self.id_deb)
+
 
 
 
@@ -234,12 +300,13 @@ def print_session():
 
 def select_obj(value, param1, param2, param3, param4):
     b = []
-    if param3 == None:
+
+    if param4 == None:
         for value_c in session.query(value).filter(param1 == param2):
-            b.append(str(value_c[0]))
+            b.append(value_c[0])
     else:
         for value_c in session.query(value).filter(param1 == param2).filter(param3 == param4):
-            b.append(str(value_c[0]))
+            b.append(value_c[0])
     return b
 
 
